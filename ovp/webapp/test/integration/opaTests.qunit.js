648833a63a9b52d31fb8b80168279d2fc8dbe702
Kevin Dass <kdass@mast.com>
2023-03-02 11:48:09 +0000

    first commit

---
+++ ovp/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["com/lb/flp/tracker/ovp/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ ovp/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"comlbflptracker/ovp/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
